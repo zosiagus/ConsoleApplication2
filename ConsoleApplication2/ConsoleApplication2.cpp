f1ed0ff2d8b7caeee80558b49c28fb78ed9eeb0b
student <student@LAB-45-5>
2025-10-21 14:52:28 +0200

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,49 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{    //zad 1 
+
+
+	//cout << "ZZZZZZZZ       GG" << endl;
+	//cout << "   ZZ          GG" << endl;
+	//cout << "   ZZ          GG" << endl;
+	//cout << "   ZZ          GG" << endl;
+	//cout << "   ZZ          GG" << endl;
+	//cout << "   ZZ          GG" << endl;
+
+	//zad2
+
+	//cout << "     *     " << endl;
+	//cout << " **  *   **" << endl;
+	//cout << "   * * *  "<< endl;
+	//cout << " *** * *** " << endl;
+	//cout << "   *   *   " << endl;
+
+	// zad 3 
+
+    cout << "--------------------" << endl;
+	cout << "  LISTA OBECNOSCI   " << endl;
+	cout << "--------------------" << endl;
+	cout << "Nr   Imie   Nazwisko" << endl;
+	cout << "--------------------" << endl;
+	cout << "1.   Jan    Kowalski" << endl;
+	cout << "--------------------" << endl;
+	cout << "2.   Anna   Nowak   " << endl;
+	cout << "--------------------" << endl;
+	cout << "3.   Piotr Zielinski" << endl;
+	cout << "--------------------" << endl;
+
+	// zad 4 
+	int a, b;
+
+
+
+
+
+} 
+
+   
