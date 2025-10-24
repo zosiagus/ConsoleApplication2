5f671eb914d29c7bc4eb2a9fc1ec01474a1025d1
zosiagus <zosia.gus@gmail.com>
2025-10-24 21:35:13 +0200

    lab1

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -25,20 +25,53 @@ int main()
 
 	// zad 3 
 
-    cout << "--------------------" << endl;
-	cout << "  LISTA OBECNOSCI   " << endl;
-	cout << "--------------------" << endl;
-	cout << "Nr   Imie   Nazwisko" << endl;
-	cout << "--------------------" << endl;
-	cout << "1.   Jan    Kowalski" << endl;
-	cout << "--------------------" << endl;
-	cout << "2.   Anna   Nowak   " << endl;
-	cout << "--------------------" << endl;
-	cout << "3.   Piotr Zielinski" << endl;
-	cout << "--------------------" << endl;
+    //cout << "--------------------" << endl;
+	//cout << "  LISTA OBECNOSCI   " << endl;
+	//cout << "--------------------" << endl;
+	//cout << "Nr   Imie   Nazwisko" << endl;
+	//cout << "--------------------" << endl;
+	//cout << "1.   Jan    Kowalski" << endl;
+	//cout << "--------------------" << endl;
+	//cout << "2.   Anna   Nowak   " << endl;
+	//cout << "--------------------" << endl;
+	//cout << "3.   Piotr Zielinski" << endl;
+	//cout << "--------------------" << endl;
 
-	// zad 4 
-	int a, b;
+	// zad 4 a)
+	
+	//int a, b;
+	//cout << "podaj pierwsza liczbe: ";
+	//cin >> a;
+	//cout << "podaj druga liczbe:  ";
+	//cin >> b;
+	//int c = a + b;
+	//cout << "suma tych liczb to: " << c << endl;
+	//int d = a * b;
+	//cout << "iloczyn tych liczb to: " << d;
+
+	//zad 4 b)
+
+	//float a, b;
+	//cout << "podaj pierwsza liczbe: ";
+	//cin >> a;
+	//cout << "podaj druga liczbe: ";
+	//cin >> b;
+	//float c = a + b;
+	//cout << "suma tych liczb to: " << c << endl;
+	//float d = a * b;
+	//cout << "iloczyn tych liczb to: " << d;
+
+	//zad 5
+
+	int f, c;
+	cout << "podaj temperature w stopniach Fahrenheita: ";
+	cin >> f;
+	c = 5 * (f - 32) / 9;
+	cout << f << " stopni Fahrenheita to " << c << " stopni Celsjusza" << endl;
+
+	
+
+
 
 
 
